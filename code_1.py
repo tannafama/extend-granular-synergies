2599f0c6a57001708552041fb390e2b7d0abfff8
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-10-26 10:34:46 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,43 @@
+    return data
+import random
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+
+
+        print(f"Random Number: {item}")
+def main():
+def generate_random_data():
+if __name__ == "__main__":
+    data = generate_random_data()
+if __name__ == "__main__":
+    for item in data:
+        print(f"Random Number: {item}")
+
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
+    main()
+
+
+
+import randommain();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+function main() {
+        console.log("Random Number: " + item);
+    return data;
+}
+    });
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    }
+
+function generateRandomData() {
+    const data = generateRandomData();
+
+    data.forEach(item => {
